56d855936cef93fc3100a0263939c633265e81bb
ChazBeck <charlie@becknexus.com>
2026-01-02 08:01:00 -0600

    Add temporary error display for debugging 500 errors

--- apps/admin/hours-log.php
+++ apps/admin/hours-log.php
@@ -1,4 +1,9 @@
 <?php
+// Temporary error display for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /**
  * Hours Log - View and Edit All Hours
  * 
